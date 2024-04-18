87b30f57c8261ec08cc197cce0037935e60c559e
madarazed <madarazeduchiha@gmail.com>
2024-04-18 09:44:45 -0500

    mi primer commit

---
+++ ejercicio.py
@@ -0,0 +1,24 @@
+a=2
+b=5
+suma=a+b
+print ( "la suma es ",suma)
+
+a=7
+b=2
+resta=a-b
+print ( "la resta es",resta)
+
+a=10
+b=5
+multiplicacion=a*b
+print ( " la multiplicacion ", multiplicacion )
+
+a=15
+b=5
+division=a/b
+print ("la division es: ", division)
+
+a=15
+b=5
+division=a%b
+print ("la division es: ", division)
